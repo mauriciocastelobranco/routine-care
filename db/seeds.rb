008f89a1e2f36d9adc323e0cd3d6cc143998ba2b
mauricio castelo branco <mauriciocastelobranco@gmail.com>
2025-12-08 16:19:59 -0300

    primeiros seeds feitos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Medication.destroy_all
+Patient.destroy_all
+User.destroy_all
+
+User.create!(email: "teste@email.com", password: "123123")
+
+joao = Patient.create!(name: "João Grilo", birth: "01/02/03", user_id: User.last.id)
+maria = Patient.create!(name: "Maria José", birth: "02/03/04", user_id: User.last.id)
+enzo = Patient.create!(name: "Enzo Souza", birth: "03/04/05", user_id: User.last.id)
+
+m1 = Medication.create!(name: "Vitamina D", dosage: "1 cápsula semanal", patient_id: joao.id)
+
+m2 = Medication.create!(
+  name: "Omeprazol 20mg",
+  dosage: "1 cápsula por dia em jejum",
+  patient_id: maria.id
+  )
+
+m3 = Medication.create!(
+  name: "Dipirona 500mg",
+  dosage: "1 comprimido a cada 6 horas em caso de dor",
+  patient_id: enzo.id
+)
+
+m4 = Medication.create!(
+  name: "Amoxicilina 875mg",
+  dosage: "1 comprimido a cada 12 horas por 7 dias",
+  patient_id: joao.id
+)
+
+m5 = Medication.create!(
+  name: "Losartana 50mg",
+  dosage: "1 comprimido de uso contínuo pela manhã",
+  patient_id: maria.id
+)
+
+m6 = Medication.create!(
+  name: "Ibuprofeno 600mg",
+  dosage: "1 comprimido a cada 8 horas após as refeições",
+  patient_id: enzo.id
+)
