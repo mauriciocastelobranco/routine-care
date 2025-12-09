68de4299e5f9b88b48d28017c98245a9ae50ed0f
mauricio castelo branco <mauriciocastelobranco@gmail.com>
2025-12-09 16:27:12 -0300

    patient index e show funcionando

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,20 @@
 class AppointmentsController < ApplicationController
+
+  def show
+    @appointment = current_user.appointments.find(params[:id])
+  end
+
+  def create
+    @patient = Patient.find(params[:patient_id])
+    @appointment = Appointment.new(title: Chat::DEFAULT_TITLE)
+    @appointment.patient = @appointment
+    @appointment.user = current_user
+
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      @appointments = @title.appointments.where(user: current_user)
+      render "patients/show"
+    end
+  end
 end
